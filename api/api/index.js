6d59735cfe0dc8fb2709f2210c3335ff8ff4f20f
BrightTheBackpack <100brightli@gmail.com>
2025-07-18 10:19:59 -0700

    add firebase

--- api/api/index.js
+++ api/api/index.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const initializeApp = require('firebase/app').initializeApp;
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+}
 const app = express()
 const port = 3000;
 
